caa4faa7251fc965fe1bd0a60e31563848050402
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-08-02 14:46:01 +0200

    [feature] Seed data for the database (Taylor Swift and prime numbers)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+taylor = Artist.create!(
+  email: "taylor.swift@example.com",
+  first_name: "Taylor",
+  last_name: "Swift"
+)
+
+Item.create!(
+  [
+    {
+      title: "Red (Taylor's Version)",
+      description: "Loving him is like driving a new Maserati down a dead-end street...",
+      artist: taylor,
+      image_url: "https://static.wikia.nocookie.net/taylor-swift/images/9/93/Red_%28Taylor%27s_Version%29.jpeg/revision/latest/scale-to-width-down/1000?cb=20210618181243"
+    },
+    {
+      title: "All Too Well (Taylor's Version)",
+      description: "It was rare, I was there, I remember it all too well",
+      artist: taylor,
+      image_url: "https://static.wikia.nocookie.net/taylor-swift/images/9/93/Red_%28Taylor%27s_Version%29.jpeg/revision/latest/scale-to-width-down/1000?cb=20210618181243"
+    },
+    {
+      title: "We Are Never Ever Getting Back Together (Taylor's Version)",
+      description: "You go talk to your friends, talk to my friends, talk to me",
+      artist: taylor,
+      image_url: "https://static.wikia.nocookie.net/taylor-swift/images/9/93/Red_%28Taylor%27s_Version%29.jpeg/revision/latest/scale-to-width-down/1000?cb=20210618181243"
+    },
+    {
+      title: "Begin Again (Taylor's Version)",
+      description: "But on a Wednesday in a café, I watched it begin again",
+      artist: taylor,
+      image_url: "https://static.wikia.nocookie.net/taylor-swift/images/9/93/Red_%28Taylor%27s_Version%29.jpeg/revision/latest/scale-to-width-down/1000?cb=20210618181243"
+    }
+  ]
+)
+
+primes = Artist.create!(
+  email: "prime.bear@example.com",
+  first_name: "Prime",
+  last_name: "Bear"
+)
+
+Item.create!(
+  Prime.take(2000).each_with_index.map do |p, idx|
+    {
+      title: "Prime nr #{idx}",
+      description: "The #{idx}st prime is #{p}",
+      artist: primes
+    }
+  end)
