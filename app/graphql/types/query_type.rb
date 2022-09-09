8ab91209664936814e0b52bbde48c83e977f1c88
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-09-09 12:26:27 +0200

    [feature] Serve Items in GraphQL

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,11 +4,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+      field :items,
+      [Types::ItemType],
+      null: false,
+      description: "Return a list of items"
+
+      def items
+        Item.all
+      end
   end
 end
