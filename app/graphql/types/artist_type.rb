0e0d654be7f3046c470e7b3efcbbc673c7f1519e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-09-09 12:23:01 +0200

    [autogen] GraphQL types
    ```
    ./bin/rails g graphql:object item
    ./bin/rails g graphql:object artist
    ```

---
+++ app/graphql/types/artist_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class ArtistType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String
+    field :last_name, String
+    field :email, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
---
+++ app/graphql/types/item_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class ItemType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String
+    field :description, String
+    field :image_url, String
+    field :artist_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
