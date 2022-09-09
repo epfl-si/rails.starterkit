c100d5477733b31c30699b5961d0712b248c551a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-09-09 22:26:20 +0200

    [feature] CSS spinner
    Zero dependencies, zero assets.

--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import { Loading } from "./spinner";
 
 export interface AppProps {
   arg: string;
 }
 
 export function App({ arg }: AppProps) {
-  return <div>{`Hello, ${arg}!`}</div>;
-};
+  return <div>{`Hello, ${arg}!`}
+      <Loading/>;
+  </div>;
+}
