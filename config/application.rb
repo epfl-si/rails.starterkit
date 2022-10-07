5bdb0143b58a8dc19f311f8fa213212df3d45748
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-07 12:11:46 +0200

    [unfeature] Cookies!
    This is now a cookie-free Web app — CSRF protection is superseded by OIDC token checking.
    
    - Configure the app globally as per https://stackoverflow.com/a/52214503
    - Tell `GraphiQL::Rails` not to attempt to use CSRF

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module HelloRails
     config.assets.configure do |env|
       SprocketsRequireInGemExtension::inject_for_javascript(env)
     end
+
+    # This is a cookie-free Web app!
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.session_store :disabled
+    if Rails.env.development?
+      GraphiQL::Rails.config.csrf = false
+    end
   end
 end
