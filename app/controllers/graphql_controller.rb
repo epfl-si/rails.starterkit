56aac874b52e01505e8b24011021bf98e97b4f50
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-07 12:03:39 +0200

    [refactor] Introduce `APIController` intermediate parent class
    This is the placeholder for the future authentication concerns.

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,9 +1,4 @@
-class GraphqlController < ApplicationController
-  # If accessing from outside this domain, nullify the session
-  # This allows for outside API access while preventing CSRF attacks,
-  # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
-
+class GraphqlController < APIController
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
