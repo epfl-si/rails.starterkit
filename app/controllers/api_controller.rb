dcbeb28572b7b447e66a0c169c271067af50eca0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-07 12:03:39 +0200

    [refactor] Introduce `APIController` intermediate parent class
    This is the placeholder for the future authentication concerns.
    
    - Use an inflection, yow! — so that `zeitwerk` (Rails 6 and onwards' new kaiser of CamelCaseIfication) knows that the `APIController` class lives in the `api_controller.rb` file.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,7 @@
+# Base class for API controllers in this app
+class APIController < ApplicationController
+  # If accessing from outside this domain, nullify the session
+  # This allows for outside API access while preventing CSRF attacks,
+  # but you'll have to authenticate your user separately
+  # protect_from_forgery with: :null_session
+end
