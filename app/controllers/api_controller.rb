2323b700c1edcae26dfcab2063ec9eeeae08acea
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-11 09:25:04 +0200

    [feature] Server-side OpenID-Connect authentication
    - Apply and configure the `warden_openid_bearer` gem
    - Enforce authentication for all subclasses of `APIController`

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,6 @@
 # Base class for API controllers in this app
 class APIController < ApplicationController
-  # If accessing from outside this domain, nullify the session
-  # This allows for outside API access while preventing CSRF attacks,
-  # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
+  before_action do
+    authenticate!
+  end
 end
