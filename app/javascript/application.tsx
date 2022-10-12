3d58f33f929fc32cd2b802da65a3c8a896cf6838
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-12 18:53:48 +0200

    [refactor] for React 18, extra component-itude
    - Bye `react-dom`, welcome `react-dom/client`
    - Rename `app/javascript/application.ts` to `app/javascript/application.tsx`, since we can
    - Move unsightly `document.addEventlistener` / `ReactDOM.render` business in there

---
+++ app/javascript/application.tsx
@@ -0,0 +1,10 @@
+// Entry point for the build script in your package.json
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+import { App } from "./components/application";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootEl = document.getElementById("react-app");
+  ReactDOM.createRoot(rootEl).render(<App arg="Rails 7 with ESBuild" />);
+});
--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -1,15 +1,9 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 
-interface AppProps {
+export interface AppProps {
   arg: string;
 }
 
-const App = ({ arg }: AppProps) => {
+export function App({ arg }: AppProps) {
   return <div>{`Hello, ${arg}!`}</div>;
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  const rootEl = document.getElementById("react-app");
-  ReactDOM.render(<App arg="Rails 7 with ESBuild" />, rootEl);
-});
