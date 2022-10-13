e0d96cb5fda7867acdeb406b5eda89f44901d610
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-10-13 11:21:51 +0200

    [feature] Make a GraphQL query on the homepage when logged in
    - Use the modern `@apollo/client`
    - Apply https://www.apollographql.com/docs/react/networking/authentication/#header (and a helper class) to pass in the OpenID-Connect access token
    - Render it with `@tanstack/react-table`

--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -1,8 +1,34 @@
 import * as React from "react";
+import { useRef } from "react";
 import { useAsyncEffect } from "use-async-effect";
 import { fetchOIDCConfig } from "../server/openid_configuration";
 import { OIDCContext, StateEnum, LoginButton, IfOIDCState, LoggedInUser } from "@epfl-si/react-appauth";
+
+import { createHttpLink, ApolloClient, InMemoryCache, ApolloProvider, NormalizedCacheObject, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ItemList } from "./item-list";
 import { Loading } from "./spinner";
+
+class AuthState {
+  private token ?: string;
+  setToken(token : string) {
+    this.token = token;
+  }
+  logout() {
+    this.token = undefined;
+  }
+
+  // https://www.apollographql.com/docs/react/networking/authentication/#header
+  httpLink(uri) {
+    const authLink = setContext((_, { headers }) => ({
+      headers: {
+        ...headers,
+        authorization: this.token ? `Bearer ${this.token}` : ""
+      }
+    }));
+    return authLink.concat(createHttpLink({ uri }));
+  }
+}
 
 export function App() {
   const [authServerUrl, setAuthServerUrl] = React.useState<string>();
@@ -12,23 +38,32 @@ export function App() {
     if (componentIsStillLive()) setAuthServerUrl(auth_server);
   });
 
+  const { current } = useRef<{
+    auth ?: AuthState,
+    client ?: ApolloClient<NormalizedCacheObject>
+  }>({});
+
   if (! authServerUrl) return <Loading/>;
+
+  if (! current.auth) current.auth = new AuthState();
+  if (! current.client) current.client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: current.auth.httpLink('/graphql')
+  });
+
   return <OIDCContext authServerUrl = { authServerUrl }
                       client = { { clientId: "hello_rails",
                                    redirectUri: "http://localhost:3000/" } }
-                      onNewToken = { onNewToken }
-                      onLogout = { onLogout }  >
+                      onNewToken = { (token) => { current.auth.setToken(token); } }
+                      onLogout = { () => { current.auth.logout(); } }  >
     <LoginButton inProgressLabel={ <Loading/> }/>
     <IfOIDCState is={ StateEnum.LoggedIn }>
       <p>Hello, <LoggedInUser field="preferred_username" />!</p>
     </IfOIDCState>
+      <ApolloProvider client={ current.client }>
+        <IfOIDCState is={ StateEnum.LoggedIn }>
+        <ItemList/>
+        </IfOIDCState>
+      </ApolloProvider>
     </OIDCContext>;
-}
-
-function onNewToken(token : string) {
-  console.log("Received new access token");
-}
-
-function onLogout() {
-  console.log("Logged out");
 }
