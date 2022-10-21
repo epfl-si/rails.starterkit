650fb87d4ff2e01b7922c0e811d1ff966d84c9c3
SaphireVert <jerome.cosandey@gmail.com>
2022-10-21 15:34:24 +0200

    [feature] Generalise mutation

--- app/graphql/mutations/update_artist.rb
+++ app/graphql/mutations/update_artist.rb
@@ -2,15 +2,14 @@ module Mutations
   class UpdateArtist < BaseMutation
     # arguments passed to the `resolve` method
     argument :id, String, required: true
-    argument :firstName, String, required: true
+    argument :first_name, String, required: true
 
     # return type from the mutation
     type Types::ArtistType
 
     def resolve(input)
       artist=Artist.find(input[:id])
-      artist.first_name = input[:firstName]
-      artist.save
+      artist.update! input
       artist
     end
   end
